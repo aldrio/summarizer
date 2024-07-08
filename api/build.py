243efa610c4691bcbd28be4d4412a495868a75e4
Brandon Aldrich <brandon@aldr.io>
2024-07-08 14:26:33 -0400

    fix wrong dir

--- api/build.py
+++ api/build.py
@@ -1,4 +1,4 @@
 import nltk
 
-nltk.download("punkt", download_dir="./nltk")
-nltk.download("stopwords", download_dir="./nltk")
+nltk.download("punkt", download_dir="./nltk_data")
+nltk.download("stopwords", download_dir="./nltk_data")
