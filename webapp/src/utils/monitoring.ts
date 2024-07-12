f007c62df33fec8dead86ae70b61ee0e7f7b8888
Brandon Aldrich <brandon@aldr.io>
2024-07-12 12:52:37 -0400

    remove async import

--- webapp/src/utils/monitoring.ts
+++ webapp/src/utils/monitoring.ts
@@ -1,5 +1,6 @@
+import Sentry from "@sentry/react";
+
 if (import.meta.env.PROD && import.meta.env.SSR === false) {
-  const Sentry = await import("@sentry/react");
   Sentry.init({
     dsn: "https://3b863adc28655f68ce8832a4f747668b@o555244.ingest.us.sentry.io/4507589829394432",
     integrations: [],
